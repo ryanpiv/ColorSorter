8d5993602fe6af9240b55fa00203d1170ccad295
ryanpiv <ryanpiveteau@gmail.com>
2020-04-25 19:20:50 -0400

    modified base cra, fixed package json issues

---
+++ src/ColorSorter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ColorSorter() {
+  return (
+    <h1>hello </h1>
+  );
+}
+
+export default ColorSorter;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/main.scss';
+import ColorSorter from './ColorSorter';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ColorSorter />
   </React.StrictMode>,
   document.getElementById('root')
 );
