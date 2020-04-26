5c6423eabf1dbc38798c5e663bd10e797c5d627b
ryanpiv <ryanpiveteau@gmail.com>
2020-04-26 18:28:18 -0400

    Updates

---
+++ precache-manifest.a9fcce1f5587fad3a7f1ca88a28a727e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcba92d13220970abea5fc0dddd4c008",
+    "url": "/color-sorter/index.html"
+  },
+  {
+    "revision": "07eb94a90619887ec4cd",
+    "url": "/color-sorter/static/css/main.40ce88bb.chunk.css"
+  },
+  {
+    "revision": "42c6429a37f40d7aa5e1",
+    "url": "/color-sorter/static/js/2.b1f7a776.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/color-sorter/static/js/2.b1f7a776.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07eb94a90619887ec4cd",
+    "url": "/color-sorter/static/js/main.158ba644.chunk.js"
+  },
+  {
+    "revision": "3636bfbf6fa059e45f48",
+    "url": "/color-sorter/static/js/runtime-main.8aa9387b.js"
+  },
+  {
+    "revision": "43cbf41030f7d98f7f0a6b72d463777d",
+    "url": "/color-sorter/static/media/icon-settings.43cbf410.svg"
+  }
+]);
